6e3024507d315ebf7c94ff338a5863943162572f
vivinchandrra1976 <151639242+vivinchandrra1976@users.noreply.github.com>
2023-11-21 21:11:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,10 +57,10 @@ function checkAgeValidity() {
  
     const table= `<table class="table-auto w-full"><tr>
      <th class="px-4 py-2">Name</th>
-     <th class="px-4 py-2">Email </th>
+     <th class="px-4 py-2">Email</th>
      <th class="px-4 py-2">Password</th>
      <th class="px-4 py-2">Dob</th>
-     <th class="px-4 py-2">Accepted terms</th>
+     <th class="px-4 py-2">Accepted terms?</th>
      </tr>${tableEntries} </table>`;
      let details=document.getElementById("user-entries");
      details.innerHTML = table;
